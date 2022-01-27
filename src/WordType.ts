bdd4b8e875125041c8430b234b660959752602fc
neko <ai73a@yandex.ru>
2022-01-27 17:11:16 +0300

    l1

---
+++ src/WordType.ts
@@ -0,0 +1,12 @@
+export type WordType = {
+    ru: string
+    en: string
+    tr: string // voice
+    de: string // about
+    er: string[] // errors
+}
+
+// перевод
+// слух
+// картинка
+// пара
